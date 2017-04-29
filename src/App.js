0b0eb51444fcbceae70ef8c8caf3500902bc8df7
zacscodingclub <zbaston@gmail.com>
2017-04-29 14:30:44 -0400

    remove default CSS, setup main app div

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Image, CloudinaryContext, Transformation } from 'cloudinary-react';
 import './App.css';
 
+const ImageTransformations = ({ width, selectedShirt }) => {
+  return (
+    <Image publicId={selectedShirt.main+'.jpg'}>
+      <Transformation width={width} crop="scale" />
+    </Image>
+  );
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    const defaultShirt = { id: 1, main: 'shirt_only' };
+    this.state = {
+      shirts: [
+        defaultShirt,
+        { id: 2, main: 'laying-shirt' },
+        { id: 3, main: 'hanging_t-shirt' }
+      ],
+      selectedShirt: defaultShirt
+    };
+  }
+
+  selectShirt(thumb) {
+    this.setState({ selectedShirt: thumb}, _ => this.forceUpdate());
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CloudinaryContext cloudName="{process.env.REACT_APP_CLOUD_NAME}">
+          <div id="imageDemoContainer">
+            <div id="mainImage">
+              <ImageTransformations
+                        width="600"
+                        rgb={rgb}
+                        selectedShirt={this.state.selectedShirt}
+                        text={this.state.text} />
+            </div>
+            <div id="imageThumbs">
+              <ul id="thumbs">
+                {this.state.shirts.map(thumb => {
+                  return (
+                    <li>
+                      <ImageTransformations
+                                width="75"
+                                rgb={rgb}
+                                selectedShirt={thumb}
+                                text={' '} />
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+        </CloudinaryContext>
       </div>
     );
   }
